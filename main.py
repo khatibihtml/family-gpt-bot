d3ee0ef3e9192dcc02cd3c6159213a61d2713ac6
khatibihtml <hadinorpil@gmail.com>
2025-11-09 02:30:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,6 @@ app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     print("🤖 ربات با OpenRouter در حال اجراست...")
     app.run_polling()
